38bc2b5887752b6f5b2b4d8a750ba7e24f61eefe
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2022-04-06 08:46:09 -0700

    Initial Commit

---
+++ AlpineConnect/AlpineConnect.h
@@ -0,0 +1,18 @@
+//
+//  AlpineConnect.h
+//  AlpineConnect
+//
+//  Created by Jenya Lebid on 4/6/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlpineConnect.
+FOUNDATION_EXPORT double AlpineConnectVersionNumber;
+
+//! Project version string for AlpineConnect.
+FOUNDATION_EXPORT const unsigned char AlpineConnectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlpineConnect/PublicHeader.h>
+
+
