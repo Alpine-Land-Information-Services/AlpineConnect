f626c971beb9ec33d4c16975c1e956bbaeed73ed
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2022-04-27 14:04:21 -0700

    Created Swift Package

---
+++ Sources/AlpineConnect/AlpineConnect.swift
@@ -0,0 +1,6 @@
+public struct AlpineConnect {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/AlpineConnectTests/AlpineConnectTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AlpineConnect
+
+final class AlpineConnectTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AlpineConnect().text, "Hello, World!")
+    }
+}
