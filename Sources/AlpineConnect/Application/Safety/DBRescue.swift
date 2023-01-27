f8997e9683d02e96b826f23d797bc815fd94b9eb
mkv <kons@devake.com>
2023-01-27 20:20:01 +0600

    #upload routine

--- Sources/AlpineConnect/Application/Safety/DBRescue.swift
+++ Sources/AlpineConnect/Application/Safety/DBRescue.swift
@@ -7,6 +7,9 @@
 
 import CoreData
 import AlpineUI
+import Zip
+import PostgresClientKit
+import UIKit
 
 public class DBRescue {
     
@@ -64,5 +67,72 @@ public class DBRescue {
          } catch {
              AppControl.makeError(onAction: "Clearing Map Data", error: error)
          }
+    }
+    
+    static func RescueDB(userName: String, url: URL, handler: @escaping ((Bool, String) -> Void)) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                let tempUrl = try Zip.quickZipFiles([url], fileName: "tempDB")
+                let data = try Data(contentsOf: tempUrl)
+
+                DispatchQueue.main.async {
+                    defer { try? FileManager.default.removeItem(at: tempUrl) }
+                    SendArchiveData(userName: userName, data: data, handler: { finished, text in
+                        handler(finished, text)
+                        print("Database Sent")
+                    })
+                }
+            }
+            catch {
+                handler(false, "\(error)")
+                print(error)
+            }
+        }
+    }
+
+    static func SendArchiveData(userName: String, data: Data, handler: @escaping ((Bool, String) -> Void)) {
+        TrackingManager.shared.pool?.withConnection { con_from_pool in
+            do {
+                let appName = Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String ?? "Unknown"
+                let connection = try con_from_pool.get()
+                defer { connection.close() }
+                let query1 = """
+                    INSERT INTO public.recovery_files(
+                    id
+                    , application
+                    , user_name
+                    , data
+                    , upload_date)
+                    VALUES (
+                      '\(UIDevice.current.identifierForVendor ?? UUID())'
+                    , '\(appName)'
+                    , '\(userName)'
+                    , '\(PostgresByteA.init(data: data).postgresValue)'
+                    , '\(DatetoStringTimeZonePST())')
+
+                    ON CONFLICT (id, application) DO UPDATE SET
+                    application = EXCLUDED.application,
+                    user_name = EXCLUDED.user_name,
+                    data = EXCLUDED.data,
+                    upload_date = EXCLUDED.upload_date
+                    """
+                let statement1 = try connection.prepareStatement(text: query1)
+                defer { statement1.close() }
+                try statement1.execute()
+
+                handler(true, "Export Successful")
+            }
+            catch {
+                handler(false, "\(error)")
+                print(error)
+            }
+        }
+    }
+    
+    static func DatetoStringTimeZonePST(date: Date = Date()) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.timeZone = TimeZone(identifier: "America/Los_Angeles")!
+        return dateFormatter.string(from: date)
     }
 }
