062d17c4d3dbda7a69ea892d1ac3a35b4e964e44
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2023-03-01 15:53:53 -0800

    Create Notification.swift

---
+++ Sources/AlpineConnect/Extensions/Notification.swift
@@ -0,0 +1,13 @@
+//
+//  Notification.swift
+//  AlpineConnect
+//
+//  Created by Jenya Lebid on 3/1/23.
+//
+
+import Foundation
+
+public extension Notification.Name {
+    
+    static let mapObjectSelect = Notification.Name(rawValue: "AMK_Map_Object_Select")
+}
