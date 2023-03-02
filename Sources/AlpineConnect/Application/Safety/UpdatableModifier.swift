5f851643d6972df39e00f6adfc4ab30e6f003a59
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2023-03-02 15:50:52 -0800

    Update UpdatableModifier.swift

--- Sources/AlpineConnect/Application/Safety/UpdatableModifier.swift
+++ Sources/AlpineConnect/Application/Safety/UpdatableModifier.swift
@@ -10,19 +10,31 @@ import SwiftUI
 struct UpdatableModifier: ViewModifier {
     
     @State var localID = UUID()
-
+    var id: String
+    
     func body(content: Content) -> some View {
         content
             .id(localID)
-            .onReceive(NotificationCenter.default.publisher(for: NSNotification.Name("Connect-Refresh"))) { _ in
-                localID = UUID()
+            .onReceive(NotificationCenter.default.publisher(for: NSNotification.Name("View_Update"))) { id in
+                guard let id = id.userInfo?.first?.value as? String else {
+                    return
+                }
+                if id == self.id {
+                    localID = UUID()
+                }
             }
     }
 }
 
 public extension View {
-    var updatable: some View {
-        modifier(UpdatableModifier())
+    func updateable(_ id: String) -> some View {
+        modifier(UpdatableModifier(id: id))
+    }
+}
+
+public extension Notification {
+    static func viewUpdate(with id: String) -> Notification {
+        Notification(name: Notification.Name("View_Update"), object: nil, userInfo: ["id": id])
     }
 }
 
