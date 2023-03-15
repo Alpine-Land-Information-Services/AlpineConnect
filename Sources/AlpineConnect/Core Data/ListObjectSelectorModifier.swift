58d54ed69342ea5a533d87b9537388ec20af7099
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2023-03-15 16:00:48 -0700

    Update ListObjectSelectorModifier.swift

--- Sources/AlpineConnect/Core Data/ListObjectSelectorModifier.swift
+++ Sources/AlpineConnect/Core Data/ListObjectSelectorModifier.swift
@@ -7,56 +7,55 @@
 
 import SwiftUI
 
-struct ListObjectSelectorModifier<Destination: View>: ViewModifier {
+struct ListObjectSelectorModifier: ViewModifier {
     
-    var name: String
+//    var name: String
     
-    @State var open: Bool = false
+//    @State var open: Bool = false
+//
+//    @State var destination: Destination
+//    @State var action: (() -> ())?
     
-    @State var destination: Destination
-    @State var action: (() -> ())?
-    
-    init(_ name: String, @ViewBuilder destination: () -> Destination, action: (() -> ())?) {
-        self.name = name
-        self._destination = State(wrappedValue: destination())
-        if let action {
-            self._action = State(wrappedValue: action)
-        }
-    }
+//    init(_ name: String, @ViewBuilder destination: () -> Destination, action: (() -> ())?) {
+//        self.name = name
+//        self._destination = State(wrappedValue: destination())
+//        if let action {
+//            self._action = State(wrappedValue: action)
+//        }
+//    }
     
     func body(content: Content) -> some View {
         ScrollViewReader { value in
             content
-                .onAppear {
-                    checkAndOpen(proxy: value)
+                .onReceive(NotificationCenter.default.publisher(for: Notification.Name("ListItemSelect"))) { info in
+                    if let id = info.userInfo?.first?.value as? UUID {
+                        value.scrollTo(id, anchor: .center)
+                    }
                 }
-                .background(
-                    NavigationLink(isActive: $open, destination: {
-                        destination
-                    }, label: {
-                        EmptyView()
-                    })
-                )
         }
     }
     
-    func checkAndOpen(proxy: ScrollViewProxy) {
-        if let object = ListObjectSelector.object(with: name) {
-            proxy.scrollTo(object.guid, anchor: .center)
-            if let action {
-                action()
-            }
-            else {
-                open = true
-            }
-            NotificationCenter.default.post(name: Notification.Name("ListItemSelect"), object: nil, userInfo: ["id": object.guid])
-            ListObjectSelector.removeObject(with: name)
-        }
-    }
+//    func checkAndOpen(proxy: ScrollViewProxy) {
+//        if let object = ListObjectSelector.object(with: name) {
+//            proxy.scrollTo(object.guid, anchor: .center)
+//            if let action {
+//                action()
+//            }
+//            else {
+//                open = true
+//            }
+//            NotificationCenter.default.post(name: Notification.Name("ListItemSelect"), object: nil, userInfo: ["id": object.guid])
+//            ListObjectSelector.removeObject(with: name)
+//        }
+//    }
 }
 
 public extension View {
-    func listObjectOpener<Destination: View>(for name: String, @ViewBuilder destination: () -> Destination = {EmptyView()}, action: (() -> ())? = nil) -> some View {
-        modifier(ListObjectSelectorModifier(name, destination: destination, action: action))
+//    func listObjectOpener<Destination: View>(for name: String, @ViewBuilder destination: () -> Destination = {EmptyView()}, action: (() -> ())? = nil) -> some View {
+//        modifier(ListObjectSelectorModifier(name, destination: destination, action: action))
+//    }
+    
+    var listSelector: some View {
+        modifier(ListObjectSelectorModifier())
     }
 }
